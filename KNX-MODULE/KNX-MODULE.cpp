6f4529fc9db922951fc73fbed522c886b8bc282a
delete-ui <papdzhon@yandex.ru>
2024-08-20 23:06:27 +0300

    Add files via upload

---
+++ KNX-MODULE/KNX-MODULE.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <chrono>
+#include <thread>
+//#include "knxpp/knx.hpp" This is an example code but not a real implementation, because I don't have a knx module)
+
+using namespace std;
+
+// fake module IDs
+const int TEMPERATURE_SENSOR_ID = 1;
+const int SHUTTER_ACTUATOR_ID = 2;
+const int LIGHTING_ACTUATOR_ID = 3;
+
+const int LOWER_TEMP_THRESHOLD = 18; // Lower temperature threshold
+const int HIGHER_TEMP_THRESHOLD = 22; // Upper temperature threshold
+
+int getTemperature() 
+{
+    // We get the current temperature from the climate sensor (replace it with a real challenge)
+    return read_value(TEMPERATURE_SENSOR_ID);
+}
+
+void controlShutters(int temperature) 
+{
+    if (temperature < LOWER_TEMP_THRESHOLD)
+    {
+        // Opening the curtains
+        send_command(SHUTTER_ACTUATOR_ID, "open");
+    }
+    else
+    {
+        // Closing the curtains
+        send_command(SHUTTER_ACTUATOR_ID, "close");
+    }
+}
+
+void adjustLighting(int temperature) 
+{
+
+    if (temperature < LOWER_TEMP_THRESHOLD) 
+    {
+        // Making the lighting brighter
+        send_command(LIGHTING_ACTUATOR_ID, "brightness, high");
+    }
+    else {
+        // Making the lighting more languid
+        send_command(LIGHTING_ACTUATOR_ID, "brightness, low");
+    }
+}
+
+int main() 
+{
+    while (true) 
+    {
+        int current_temperature = getTemperature();
+
+        controlShutters(current_temperature);
+        adjustLighting(current_temperature);
+
+        // The delay before the next cycle of sensor polling at competitions was set from 5 to 10 seconds
+        this_thread::sleep_for(chrono::seconds(5));
+    }
+    return 0;
+}
